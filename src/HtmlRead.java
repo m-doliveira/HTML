3357c124c23f99e3e4006b4214bc8a9db61383a9
m-doliveira <monica_doliveira24@milton.edu>
2023-02-06 09:03:31 -0500

    end of class 2.6.23

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -1,15 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class HtmlRead {
+    JFrame frame;
+    int height = 500;
+    int width = 800;
+    int w = 50;
+    int h = 50;
+    JTextArea input;
+    JTextField term;
+    JTextArea output;
+    JLabel topLabel;
+    JLabel midLabel;
+    JLabel bottomLabel;
 
     public static void main(String[] args) {
         HtmlRead html = new HtmlRead();
     }
 
     public HtmlRead() {
-
+        frame = new JFrame("link_search");
+        frame.setLayout(new GridLayout(6, 1));
+        frame.setSize(width, height);
+        frame.setVisible(true);
+        topLabel = new JLabel("input URL");
+        input = new JTextArea();
+        input.setSize(w, h);
+        midLabel = new JLabel("key term");
+        term = new JTextField();
+        bottomLabel = new JLabel("results");
+        output = new JTextArea();
+        output.setSize(w, h);
+        frame.add(topLabel);
+        frame.add(input);
+        frame.add(midLabel);
+        frame.add(term);
+        frame.add(bottomLabel);
+        frame.add(output);
+        frame.setVisible(true);
         try {
             System.out.println();
             System.out.print("hello \n");
@@ -18,15 +49,18 @@ public class HtmlRead {
                     new InputStreamReader(url.openStream())
             );
             String line;
-            while ( (line = reader.readLine()) != null ) {
+            while ((line = reader.readLine()) != null) {
                 //lines that have the letter x will be printed from index 50
-                if(line.contains("https://")|| line.contains("www.")){}
-                if(line.indexOf("x",50)!=-1){
-                System.out.println(line);
+                // look into using .contains commands
+                if(line.contains("href")){
+                    System.out.println(line);
                 }
+               // if (line.indexOf("x", 50) != -1) {
+                   // System.out.println(line);
+               // }
             }
             reader.close();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.out.println(ex);
         }
 
